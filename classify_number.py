8a1ff099ba981e4bd0ac08839c6012f501b426fb
CoGrandis <grandisconstanza@gmail.com>
2026-04-06 10:48:35 -0300

    Implement classify_number function
    Implemented the classify_number function to classify integers as positive/negative and even/odd.

--- classify_number.py
+++ classify_number.py
@@ -1,5 +1,3 @@
-# ---- Funciones provistas (NO modificar) ----
-
 def is_even(n):
     """Dado un número entero n, retorna True si es par, False si es impar."""
     return n % 2 == 0
@@ -8,7 +6,6 @@ def is_positive(n):
     """Dado un número entero n, retorna True si es mayor a 0, False en caso contrario."""
     return n > 0
 
-# ---- Función a implementar ----
 
 def classify_number(n):
     """
@@ -22,4 +19,21 @@ def classify_number(n):
       - "negative odd"    (negativo e impar)
       - "zero"            (el número es 0)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    
+        
+    if is_even(n) and is_positive(n):
+       classify = "positive even"
+       
+    if not is_even(n) and is_positive(n):
+        classify = "positive odd"
+
+    if is_even(n) and not is_positive(n):
+        classify = "negative even"
+        
+    if not is_even(n) and not is_positive(n):
+        classify = "negative odd"
+
+    if n == 0:
+        classify = "zero"
+        
+    return classify
